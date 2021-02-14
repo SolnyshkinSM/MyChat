46f1fa541949cfdb91789ecc604358c8d2a64917
solnyshkinsm <s.solnyshkin@it-cron.ru>
2021-02-14 15:07:37 +0300

    Setting up the project structure

---
+++ MyChat/Helper/Print.swift
@@ -0,0 +1,12 @@
+//
+//  Print.swift
+//  MyChat
+//
+//  Created by Administrator on 14.02.2021.
+//
+
+import Foundation
+
+func printFunctionLiteral(literal: String = #function) {
+    print(literal)
+}
