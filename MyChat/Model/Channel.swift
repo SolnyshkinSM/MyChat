2bd5ded8cbbc8f7912e2a59a6ebdd60be1a0375b
solnyshkinsm <s.solnyshkin@it-cron.ru>
2021-03-28 17:58:24 +0300

    Core Data

--- MyChat/Model/Channel.swift
+++ MyChat/Model/Channel.swift
@@ -10,27 +10,41 @@ import Firebase
 
 // MARK: - Channel
 
-struct Channel {
-
+class Channel {
+    
+    static let reference = Firestore.firestore().collection("channels")
+    
     let identifier: String
     let name: String
     let lastMessage: String?
     let lastActivity: Date?
-
+    var messages: [Message] = []
+    
     enum CodingKeys: String, CodingKey {
         case identifier
         case name
         case lastMessage
         case lastActivity
     }
-
+    
     init(identifier: String, with data: [String: Any]) {
-
+        
         self.identifier = identifier
         name = data[CodingKeys.name.rawValue] as? String ?? ""
         lastMessage = data[CodingKeys.lastMessage.rawValue] as? String
-
+        
         let timestamp = data[CodingKeys.lastActivity.rawValue] as? Timestamp
         lastActivity = timestamp?.dateValue()
+        
+        
+        let reference = Channel.reference.document(identifier).collection("messages")
+        reference.getDocuments { [weak self] querySnapshot, _ in
+            if let querySnapshot = querySnapshot {
+                for document in querySnapshot.documents {
+                    let message = Message(document.data())
+                    self?.messages.append(message)
+                }
+            }
+        }
     }
 }
