3c3147ee8a985e79e67ea58b55f4a2b37c6d57ac
solnyshkinsm <s.solnyshkin@it-cron.ru>
2021-04-14 15:10:19 +0300

    FileWorkManager

--- MyChat/Classes/BusinessLogicLayer/Services/FileLoader/FileLoaderProtocol.swift
+++ MyChat/Classes/BusinessLogicLayer/Services/FileLoader/FileLoaderProtocol.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+// MARK: - StateLoader
+
+enum StateLoader: String {
+    case ready, executing, finished, cancelled
+    fileprivate var keyPath: String { return "is" + rawValue.capitalized }
+}
+
 // MARK: - FileLoaderProtocol
 
 protocol FileLoaderProtocol {
