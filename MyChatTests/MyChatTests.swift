f399defd8204864e0b7c78fb97ec322c0a8399d3
solnyshkinsm <s.solnyshkin@it-cron.ru>
2021-05-03 20:45:20 +0300

    add MyChatTests

--- MyChatTests/MyChatTests.swift
+++ MyChatTests/MyChatTests.swift
@@ -18,15 +18,12 @@ class MyChatTests: XCTestCase {
     }
 
     func testExample() throws {
+        
+        XCTAssertTrue(true)
+        
+        XCTAssertTrue(false)
+        
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
