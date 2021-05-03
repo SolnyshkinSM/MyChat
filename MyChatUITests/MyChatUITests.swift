c33d08d08654a50ab3955f825f948b969bc4c46e
solnyshkinsm <s.solnyshkin@it-cron.ru>
2021-05-03 16:28:30 +0300

    Add coverage

--- MyChatUITests/MyChatUITests.swift
+++ MyChatUITests/MyChatUITests.swift
@@ -20,11 +20,14 @@ class MyChatUITests: XCTestCase {
 
     func testExample() throws {
         
+        // Arrage
         let app = XCUIApplication()
         app.launch()
         
+        // Act
         app.navigationBars["Channels"].buttons["Profile"].tap()
         
+        // Assert
         checkExistenceDelayedElement(element: app.textFields["fullName"])
         checkExistenceDelayedElement(element: app.textFields["Detailed information"])        
     }
